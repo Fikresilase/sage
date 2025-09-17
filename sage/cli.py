2bc9d5d39f49480227a7db2c16af17b61ae71043
Fikresilase <shitubilbila@gmail.com>
2025-09-17 19:55:32 +0300

    sage folder creation working

--- sage/cli.py
+++ sage/cli.py
@@ -1,2 +1,13 @@
+# import statments
+import os
+from pathlib import Path
 def main():
     print("Sage CLI running")
+    #Ask the User for permission
+    answer = input("Do you want to proceed? (y/n): ").strip().lower()
+    if answer == 'y':
+        project_path = Path.cwd() / "sage" 
+        project_path.mkdir(parents=True, exist_ok=True)
+        print(f"'sage' folder created at: {project_path}")
+    else:
+        print("Operation cancelled by the user.")
